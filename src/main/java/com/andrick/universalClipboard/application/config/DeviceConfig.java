ae18d14f2cb74a718a616b2ec74d66ffb48aa10b
andri <avilaandrick8@gmail.com>
2026-07-24 18:59:35 -0600

    feat(config): Adds @Configuration to DeviceConfig for Spring bean scanning.

--- src/main/java/com/andrick/universalClipboard/application/config/DeviceConfig.java
+++ src/main/java/com/andrick/universalClipboard/application/config/DeviceConfig.java
@@ -5,7 +5,9 @@ import com.andrick.universalClipboard.application.use_cases.GetUserDevicesUseCas
 import com.andrick.universalClipboard.application.use_cases.RemoveDeviceUseCase;
 import com.andrick.universalClipboard.domain.ports.out.DevicesRepositoryPort;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DeviceConfig {
     @Bean
     public AddDeviceUseCase addDeviceUseCase(DevicesRepositoryPort devicesRepositoryPort){
